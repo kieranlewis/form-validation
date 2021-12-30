2616137b097818218473f8da57dcfd0e74bb5a0e
Kieran Lewis <kieranlewis1334@gmail.com>
2021-12-31 09:30:13 +1300

    Added validation for type mismatch and to check if passwords match

--- script.js
+++ script.js
@@ -9,8 +9,20 @@ function checkForm() {
     for(let i = 0; i < inputs.length - 1; i++) {
         let currentInput = inputs[i];
         if(currentInput.validity.valueMissing) {
-            errorMessage += `${currentInput.name} field is missing\n`;
+            errorMessage.push(`${currentInput.name} field is missing\n`);
         }
+
+        if(currentInput.validity.typeMismatch) {
+            errorMessage.push(`${currentInput.name} should be of type ${currentInput.type}`);
+        }
+
+        if(currentInput.validity.rangeUnderFlow) {
+            errorMessage.push(`${currentInput.name} should be greater than 0`);
+        }
+    }
+
+    if(inputs[3].value != inputs[4].value) {
+        errorMessage.push('Password and confirmation should be the same');
     }
 
     if(errorMessage.length == 0) {
