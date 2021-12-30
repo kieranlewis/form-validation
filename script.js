d67edab6a12c7df21f7b6846c3e9aef3c3df6ced
Kieran Lewis <kieranlewis1334@gmail.com>
2021-12-31 09:17:50 +1300

    Checked for values missing in forms and send message to user if so

--- script.js
+++ script.js
@@ -4,11 +4,35 @@ const submitBtn = document.querySelector('button');
 const error = document.querySelector('.error');
 
 function checkForm() {
+    let errorMessage = [];
     
+    for(let i = 0; i < inputs.length - 1; i++) {
+        let currentInput = inputs[i];
+        if(currentInput.validity.valueMissing) {
+            errorMessage += `${currentInput.name} field is missing\n`;
+        }
+    }
+
+    if(errorMessage.length == 0) {
+        error.textContent = 'Hi Five';
+    } else {
+        error.textContent = errorMessage;
+    }
+
+    //Check if all inputs are valid
+    /*
+    if(inputs.every(input => input.validity.valid)) {
+        error.textContent = 'Hi Five!!!';
+    } else {
+        error.textContent = 'Errors Found';
+    }*/
 }
 
 // Event Listeners
-submitBtn.addEventListener('click', checkForm);
+submitBtn.addEventListener('click', e => {
+    e.preventDefault();
+    checkForm();
+});
 
 
 
