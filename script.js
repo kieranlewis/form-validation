83314883f53f427ec35a8ad7fb87da1a0d611a86
Kieran Lewis <kieranlewis1334@gmail.com>
2021-12-31 08:48:09 +1300

    Added submit button and place where errors will be displayed

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.forms[0];
+const inputs = [...form];
+const submitBtn = document.querySelector('button');
+const error = document.querySelector('.error');
+
+function checkForm() {
+    
+}
+
+// Event Listeners
+submitBtn.addEventListener('click', checkForm);
+
+
+
